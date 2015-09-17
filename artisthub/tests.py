44657ff907dc38ec1334438059ee03e942f3bef8
jaschneidr <jason@darkestritual.com>
2015-09-17 19:45:23 -0400

    fixed up styles quickly for the header on the artisthub side, commented out test for model dependent views. will be worked on in a new branch.

--- artisthub/tests.py
+++ artisthub/tests.py
@@ -14,9 +14,9 @@ class TestArtistHubViews(TestCase):
     def test_artisthub_root_resolves_to_artisthub_home(self):
         request = self.factory.get('/artisthub/')
         response = artisthub_home(request)
-        self.assertIn('bio-1', response.content, msg="bio-1 id not found in response.content")
+        self.assertIn('Welcome to the ArtistHub', response.content, msg="Did not find welcome message in response.content")
 
-    def test_artisthub_root_resolves_to_artist_profile(self):
-        request = self.factory.get('/artisthub/')
-        response = artisthub_home(request)
-        self.assertIn('bio-1', response.content, msg="bio-1 id not found in response.content")
+#    def test_artisthub_profile_links_resolve_to_artist_profile(self):
+#        request = self.factory.get('/artisthub/')
+#        response = artist_profile(request)
+#        self.assertIn('Artist Name:', response.content, msg="Artist Name: not found in response.content")
