ca4bb451ca498d56d0f072afb660606962035897
jaschneidr <jason@darkestritual.com>
2015-09-17 23:05:37 -0400

    removed .idea settings from git, updated reuirements.txt, updated INSTALLED_APPS to include django_extensions, refactored a bit and pared down models.py for artisthub app

--- artisthub/models.py
+++ artisthub/models.py
@@ -6,57 +6,59 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
+    TYPE_AUTHOR = 'A'
+    TYPE_VISUALARTIST = 'V'
+    TYPE_MUSICIAN = 'M'
+    TYPE_BAND = 'B'
+
+    TYPES = (
+        (TYPE_AUTHOR, 'Author'),
+        (TYPE_VISUALARTIST, 'Visual Artist'),
+        (TYPE_MUSICIAN, 'Musician'),
+        (TYPE_BAND, 'Band'),
+    )
 
     user = models.OneToOneField(User)
-    name = models.CharField(max_length=256, blank=False, null=True)
-
-
-class ProfileType(models.Model):
-
-    profile = models.ForeignKey('UserProfile', related_name='artist_types')
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length = 1, choices = TYPES, db_index = True)
     artist_name = models.CharField(max_length=140, blank=True, null=True)
     website = models.URLField(max_length=256, blank=True, null=True)
     bio = models.TextField(max_length=100000, blank=True, null=True)
 
 
-class ProfileSubtype(models.Model):
+class Musician(UserProfile):
 
-    name = models.CharField(max_length=100)
-    type = models.ForeignKey('ProfileType', related_name='subtypes')
+    genre = models.TextField(max_length=100, blank=True, null=True)
 
 
-class Musician(ProfileType):
+class Band(Musician):
 
     members = models.TextField(max_length=256, blank=True, null=True)
+    established_date = models.DateField(blank=True, null=True)
 
 
-class Author(ProfileType):
+class Author(UserProfile):
 
     pen_name = models.CharField(max_length=140, blank=True, null=True)
     dedication = models.TextField(max_length=5000, blank=True, null=True)
 
 
-class VisualArtist(ProfileType):
+class VisualArtist(UserProfile):
 
     dedication = models.TextField(max_length=5000, blank=True, null=True)
 
 
-class Instrumentalist(ProfileSubtype):
+class Instrumentalist(Musician):
 
     instrument = models.CharField(max_length=256)
 
 
-class Composer(ProfileSubtype):
+class Composer(Musician):
 
-    types_of_pieces = models.TextField(max_length=1000, blank=True, null=True)
-
-
-class SingerSongwriter(ProfileSubtype):
-
-    genre = models.CharField(max_length=60, blank=True, null=True)
+    instruments_composed_for = models.TextField(max_length=1000, blank=True, null=True)
 
 
-class Vocalist(ProfileSubtype):
+class Vocalist(Musician):
 
     range = models.CharField(max_length=60, blank=True, null=True)
 
