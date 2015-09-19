bdee82aa5b961fd4b0a02784c49a3f58e24a77e6
jaschneidr <jason@darkestritual.com>
2015-09-19 00:11:32 -0400

    Refactored ArtistHub models, especially UserProfile and Musician. Created factories for tests and tested UserProfile and Musician types/subtypes. Updated .gitignore. Updated project requirements. Created migrations initial migrations.

--- artisthub/migrations/0001_initial.py
+++ artisthub/migrations/0001_initial.py
@@ -20,6 +20,28 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
+            name='Band',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('members', models.TextField(max_length=256, null=True, blank=True)),
+                ('established_date', models.DateField(null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Composer',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('instruments_composed_for', models.TextField(max_length=1000, null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Instrumentalist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('instrument', models.CharField(max_length=256)),
+            ],
+        ),
+        migrations.CreateModel(
             name='MusicAlbum',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -28,105 +50,68 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='ProfileSubtype',
+            name='Musician',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=100)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='ProfileType',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('artist_name', models.CharField(max_length=140, null=True, blank=True)),
-                ('website', models.URLField(max_length=256, null=True, blank=True)),
-                ('bio', models.TextField(max_length=100000, null=True, blank=True)),
+                ('subtype', models.CharField(db_index=True, max_length=1, choices=[(b'B', b'Band'), (b'C', b'Composer'), (b'I', b'Instrumentalist'), (b'V', b'Vocalist')])),
+                ('genre', models.TextField(max_length=100, null=True, blank=True)),
             ],
         ),
         migrations.CreateModel(
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=256, null=True)),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+                ('name', models.CharField(max_length=100, blank=True)),
+                ('type', models.CharField(db_index=True, max_length=1, choices=[(b'A', b'Author'), (b'V', b'Visual Artist'), (b'M', b'Musician')])),
+                ('artist_name', models.CharField(max_length=140, null=True, blank=True)),
+                ('website', models.URLField(max_length=256, null=True, blank=True)),
+                ('bio', models.TextField(max_length=100000, null=True, blank=True)),
+                ('influences', models.TextField(max_length=100000, null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Vocalist',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('range', models.CharField(max_length=60, null=True, blank=True)),
             ],
         ),
         migrations.CreateModel(
             name='Author',
             fields=[
-                ('profiletype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileType')),
+                ('userprofile_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.UserProfile')),
                 ('pen_name', models.CharField(max_length=140, null=True, blank=True)),
                 ('dedication', models.TextField(max_length=5000, null=True, blank=True)),
             ],
-            bases=('artisthub.profiletype',),
-        ),
-        migrations.CreateModel(
-            name='Composer',
-            fields=[
-                ('profilesubtype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileSubtype')),
-                ('types_of_pieces', models.TextField(max_length=1000, null=True, blank=True)),
-            ],
-            bases=('artisthub.profilesubtype',),
-        ),
-        migrations.CreateModel(
-            name='Instrumentalist',
-            fields=[
-                ('profilesubtype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileSubtype')),
-                ('instrument', models.CharField(max_length=256)),
-            ],
-            bases=('artisthub.profilesubtype',),
-        ),
-        migrations.CreateModel(
-            name='Musician',
-            fields=[
-                ('profiletype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileType')),
-                ('members', models.TextField(max_length=256, null=True, blank=True)),
-            ],
-            bases=('artisthub.profiletype',),
-        ),
-        migrations.CreateModel(
-            name='SingerSongwriter',
-            fields=[
-                ('profilesubtype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileSubtype')),
-                ('genre', models.CharField(max_length=60, null=True, blank=True)),
-            ],
-            bases=('artisthub.profilesubtype',),
+            bases=('artisthub.userprofile',),
         ),
         migrations.CreateModel(
             name='VisualArtist',
             fields=[
-                ('profiletype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileType')),
+                ('userprofile_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.UserProfile')),
                 ('dedication', models.TextField(max_length=5000, null=True, blank=True)),
             ],
-            bases=('artisthub.profiletype',),
-        ),
-        migrations.CreateModel(
-            name='Vocalist',
-            fields=[
-                ('profilesubtype_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='artisthub.ProfileSubtype')),
-                ('range', models.CharField(max_length=60, null=True, blank=True)),
-            ],
-            bases=('artisthub.profilesubtype',),
+            bases=('artisthub.userprofile',),
         ),
         migrations.AddField(
-            model_name='profiletype',
+            model_name='userprofile',
+            name='user',
+            field=models.OneToOneField(to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='musician',
             name='profile',
-            field=models.ForeignKey(related_name='artist_types', to='artisthub.UserProfile'),
-        ),
-        migrations.AddField(
-            model_name='profilesubtype',
-            name='type',
-            field=models.ForeignKey(related_name='subtypes', to='artisthub.ProfileType'),
-        ),
-        migrations.AddField(
-            model_name='audioupload',
-            name='album',
-            field=models.ForeignKey(related_name='songs', to='artisthub.MusicAlbum'),
+            field=models.OneToOneField(to='artisthub.UserProfile'),
         ),
         migrations.AddField(
             model_name='musicalbum',
             name='musician',
             field=models.ForeignKey(related_name='artists_albums', to='artisthub.Musician'),
+        ),
+        migrations.AddField(
+            model_name='audioupload',
+            name='album',
+            field=models.ForeignKey(related_name='songs', to='artisthub.MusicAlbum'),
         ),
         migrations.AddField(
             model_name='audioupload',
--- artisthub/models.py
+++ artisthub/models.py
@@ -9,32 +9,32 @@ class UserProfile(models.Model):
     TYPE_AUTHOR = 'A'
     TYPE_VISUALARTIST = 'V'
     TYPE_MUSICIAN = 'M'
-    TYPE_BAND = 'B'
 
     TYPES = (
         (TYPE_AUTHOR, 'Author'),
         (TYPE_VISUALARTIST, 'Visual Artist'),
         (TYPE_MUSICIAN, 'Musician'),
-        (TYPE_BAND, 'Band'),
     )
 
     user = models.OneToOneField(User)
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length = 1, choices = TYPES, db_index = True)
+    name = models.CharField(max_length=100, blank=True)
+    type = models.CharField(max_length=1, choices=TYPES, db_index=True)
     artist_name = models.CharField(max_length=140, blank=True, null=True)
     website = models.URLField(max_length=256, blank=True, null=True)
     bio = models.TextField(max_length=100000, blank=True, null=True)
+    influences = models.TextField(max_length=100000, blank=True, null=True)
 
 
-class Musician(UserProfile):
+    def is_author(self):
+        return self.type == UserProfile.TYPE_AUTHOR
 
-    genre = models.TextField(max_length=100, blank=True, null=True)
+    def is_visual_artist(self):
+        return self.type == UserProfile.TYPE_VISUALARTIST
 
+    def is_musician(self):
+        return self.type == UserProfile.TYPE_MUSICIAN
 
-class Band(Musician):
-
-    members = models.TextField(max_length=256, blank=True, null=True)
-    established_date = models.DateField(blank=True, null=True)
+    # def get_profile_type
 
 
 class Author(UserProfile):
@@ -48,17 +48,55 @@ class VisualArtist(UserProfile):
     dedication = models.TextField(max_length=5000, blank=True, null=True)
 
 
-class Instrumentalist(Musician):
+class Musician(models.Model):
+
+    SUBTYPE_BAND = 'B'
+    SUBTYPE_COMPOSER = 'C'
+    SUBTYPE_INSTRUMENTALIST = 'I'
+    SUBTYPE_VOCALIST = 'V'
+
+    SUBTYPES = (
+        (SUBTYPE_BAND, 'Band'),
+        (SUBTYPE_COMPOSER, 'Composer'),
+        (SUBTYPE_INSTRUMENTALIST, 'Instrumentalist'),
+        (SUBTYPE_VOCALIST, 'Vocalist'),
+    )
+
+    subtype = models.CharField(max_length=1, choices=SUBTYPES, db_index=True)
+
+    profile = models.OneToOneField(UserProfile)
+    genre = models.TextField(max_length=100, blank=True, null=True)
+
+    def is_band(self):
+        return self.profile.is_musician() and self.subtype == Musician.SUBTYPE_BAND
+
+    def is_composer(self):
+        return self.profile.is_musician() and self.subtype == Musician.SUBTYPE_COMPOSER
+
+    def is_instrumentalist(self):
+        return self.profile.is_musician() and self.subtype == Musician.SUBTYPE_INSTRUMENTALIST
+
+    def is_vocalist(self):
+        return self.profile.is_musician() and self.subtype == Musician.SUBTYPE_VOCALIST
+
+
+class Band(models.Model):
+
+    members = models.TextField(max_length=256, blank=True, null=True)
+    established_date = models.DateField(blank=True, null=True)
+
+
+class Instrumentalist(models.Model):
 
     instrument = models.CharField(max_length=256)
 
 
-class Composer(Musician):
+class Composer(models.Model):
 
     instruments_composed_for = models.TextField(max_length=1000, blank=True, null=True)
 
 
-class Vocalist(Musician):
+class Vocalist(models.Model):
 
     range = models.CharField(max_length=60, blank=True, null=True)
 
@@ -68,7 +106,10 @@ class MusicAlbum(models.Model):
     name = models.CharField(max_length=256, blank=False)
     release_date = models.DateField(blank=True)
     musician = models.ForeignKey('Musician', related_name='artists_albums')
-    # album_cover
+    # direct zipped file links (aac and mp3) to be added once infrastructure has been created
+    # download_link_aac
+    # download_link_mp3
+    # album art
 
 
 class AudioUpload(models.Model):
@@ -76,4 +117,6 @@ class AudioUpload(models.Model):
     musician = models.ForeignKey('Musician', related_name='artists_songs')
     name = models.CharField(max_length=256, blank=False)
     album = models.ForeignKey('MusicAlbum', related_name='songs')
-    # file = models.CharField(max_length=512)
+    # mp3/aac audio file link to be added once infrastructure has been created
+    # download_link_aac
+    # download_link_mp3
